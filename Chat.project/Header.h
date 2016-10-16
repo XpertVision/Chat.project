2be5621d8c18c249aef32cea92bfa2de1d995fd3
Maxym Bezuglyy <maxim.bez.1990@gmail.com>
2016-10-16 21:27:32 +0300

    Add base window

---
+++ Chat.project/Header.h
@@ -0,0 +1,10 @@
+#ifndef _HEADER_H_
+#define _HEADER_H_
+
+#define MAIN_WINDOW_STYLE WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX
+
+#include <Windows.h>
+#include "resource.h"
+
+
+#endif // !_HEADER_H_
--- Chat.project/main.cpp
+++ Chat.project/main.cpp
@@ -1,4 +1,69 @@
-int main()
+#include "Header.h"
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE prevInst, LPSTR lpCmdLine, int nCmdShow)
 {
-	return 0;
+	MSG mMessage;
+	HWND hMainWnd;
+	TCHAR mainWndClassName[] = L"Chat.project";
+
+	WNDCLASSEX wMainClass;
+	ZeroMemory(&wMainClass, sizeof(wMainClass));
+
+	wMainClass.cbSize        = sizeof(wMainClass);
+	wMainClass.hInstance     = hInst;
+	wMainClass.lpfnWndProc   = MainWndProc;
+	wMainClass.lpszClassName = mainWndClassName;
+	wMainClass.cbClsExtra    = NULL;
+	wMainClass.cbWndExtra    = NULL;
+	wMainClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wMainClass.style         = CS_VREDRAW | CS_HREDRAW;
+	wMainClass.hIcon = LoadIcon(hInst, MAKEINTRESOURCE(ID_MAIN_ICO));
+
+	if (!RegisterClassEx(&wMainClass))
+	{
+		MessageBox(NULL, L"Window class don'r register", L"Error", MB_OK);
+		return 1;
+	}
+
+	hMainWnd = CreateWindowEx(NULL, mainWndClassName, L"Chat.project", MAIN_WINDOW_STYLE, CW_USEDEFAULT, CW_USEDEFAULT,
+		640, 480, NULL, NULL, hInst, NULL);
+
+	if (!hMainWnd)
+	{
+		MessageBox(NULL, L"Window don't create", L"Error", MB_OK);
+		return 1;
+	}
+
+	ShowWindow(hMainWnd, nCmdShow);
+	UpdateWindow(hMainWnd);
+
+	while (GetMessage(&mMessage, NULL, NULL, NULL))
+	{
+		TranslateMessage(&mMessage);
+		DispatchMessage(&mMessage);
+	}
+
+	return mMessage.wParam;
+}
+
+LRESULT CALLBACK MainWndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	switch (uMsg)
+	{
+	case WM_COMMAND:
+		switch (wParam)
+		{
+
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(NULL);
+		break;
+	default:
+		return DefWindowProc(hWnd, uMsg, wParam, lParam);
+	}
+
+	return NULL;
 }
